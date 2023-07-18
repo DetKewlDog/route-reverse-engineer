d01adea92303612550c430f0b5c584f18b996972
Bar Goldenstein <bargolden12@gmail.com>
2023-07-18 09:46:37 +0300

    made program look for houses instead of parkways

--- src/services/APIAccess.js
+++ src/services/APIAccess.js
@@ -4,9 +4,9 @@ class APIAccess {
     static async fetchPositionsAroundDest(destination, maxDistance) {
         const query = `[out:json];
         (
-        node["highway"="footway"](around:${maxDistance},${destination[0]},${destination[1]});
-        way["highway"="footway"](around:${maxDistance},${destination[0]},${destination[1]});
-        relation["highway"="footway"](around:${maxDistance},${destination[0]},${destination[1]});
+        node["highway"="building"](around:${maxDistance},${destination[0]},${destination[1]});
+        way["highway"="building"](around:${maxDistance},${destination[0]},${destination[1]});
+        rel["highway"="building"](around:${maxDistance},${destination[0]},${destination[1]});
         );
         out center;`
         return axios.get("https://lz4.overpass-api.de/api/interpreter", { params: { data: query } })
